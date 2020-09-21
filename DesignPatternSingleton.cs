1905bb915f11fc9082bbbc7b234ead95b5d369dd
ajunquit <ajunquit@gmail.com>
2020-09-21 00:28:57 -0500

    fisrt commit

---
+++ DesignPatternSingleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace design_pattern_singleton
+{
+
+    class ConnectionDatabase
+    {
+      
+        private string Server { get; set; }
+        private string Database { get; set; }
+        private string UserId { get; set; }
+        private string Password { get; set; }
+
+        private static ConnectionDatabase _connectionDatabase;
+
+        private ConnectionDatabase()
+        {
+            Server = "myServer";
+            Database = "myDatabase";
+            UserId = "myUserId";
+            Password = "myPassword";
+        }
+
+        public string GetConnectionString()
+        {
+            return $"Server={Server};Database={Database};User Id={UserId};Password={Password};";
+        }
+
+        public static ConnectionDatabase GetConnectionDatabase()
+        {
+            if (_connectionDatabase == null)
+            {
+                _connectionDatabase = new ConnectionDatabase();
+            }
+            return _connectionDatabase;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace design_pattern_singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // get connection for the first time.
+            ConnectionDatabase connectionSample1 = ConnectionDatabase.GetConnectionDatabase();
+            Console.WriteLine(connectionSample1.GetConnectionString());
+
+            // get connection for the second time.
+            ConnectionDatabase connectionSample2 = ConnectionDatabase.GetConnectionDatabase();
+            Console.WriteLine(connectionSample2.GetConnectionString());
+
+            Console.ReadLine();
+        }
+    }
+}
